e8d5d24661df7fa15182a5068890be6bc499323f
Behrooz Amoozad <behrooz0az@gmail.com>
2014-02-05 10:23:46 +0330

    Added license and fixed a few small errors

--- BD2.Test.Daemon.Chat/ChatAgent.cs
+++ BD2.Test.Daemon.Chat/ChatAgent.cs
@@ -1,3 +1,29 @@
+/*
+ * Copyright (c) 2014 Behrooz Amoozad
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the bd2 nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * */
 using System;
 using BD2.Daemon;
 
@@ -6,39 +32,57 @@ namespace BD2.Test.Daemon.Chat
 	public class ChatAgent:ServiceAgent
 	{
 
-		ChatAgent (ServiceAgentMode serviceAgentMode, ObjectBusSession objectBusSession)
-			:base(serviceAgentMode, objectBusSession)
+		ChatAgent (ServiceAgentMode serviceAgentMode, ObjectBusSession objectBusSession, Action flush)
+			:base(serviceAgentMode, objectBusSession, flush)
 		{
+			#if TRACE
+			Console.WriteLine (new System.Diagnostics.StackTrace (true).GetFrame (0));
+			#endif
 			objectBusSession.RegisterType (typeof(ChatMessage), ChatMessageReceived);
 		}
 
-		public static ServiceAgent CreateAgent (ServiceAgentMode serviceAgentMode, ObjectBusSession objectBusSession)
+		public static ServiceAgent CreateAgent (ServiceAgentMode serviceAgentMode, ObjectBusSession objectBusSession, Action flush)
 		{
-			return new ChatAgent (serviceAgentMode, objectBusSession);
+			#if TRACE
+			Console.WriteLine (new System.Diagnostics.StackTrace (true).GetFrame (0));
+			#endif
+			return new ChatAgent (serviceAgentMode, objectBusSession, flush);
 		}
 
 		void ChatMessageReceived (ObjectBusMessage message)
 		{
+			#if TRACE
+			Console.WriteLine (new System.Diagnostics.StackTrace (true).GetFrame (0));
+			#endif
 			ChatMessage chatMessace = (ChatMessage)message;
-			Console.WriteLine (chatMessace.Text);
+			//Console.WriteLine (chatMessace.Text);
 		}
 
 		void SendMessage (string text)
 		{
+			#if TRACE
+			Console.WriteLine (new System.Diagnostics.StackTrace (true).GetFrame (0));
+			#endif
+
 			ObjectBusSession.SendMessage (new ChatMessage (text));
 		}
 		#region implemented abstract members of ServiceAgent
 		protected override void Run ()
 		{
+			#if TRACE
+			Console.WriteLine (new System.Diagnostics.StackTrace (true).GetFrame (0));
+			#endif
 			try {
-				if (ServiceAgentMode == ServiceAgentMode.Server) {
-					return;
-				}
-				string nextMessage = MainClass.ConsoleReadLine ();
-				while (nextMessage != "/destroy") {
+				int i = 0;
+				string nextMessage = "This is a test message.";
+				DateTime t1 = DateTime.UtcNow;
+				while (i < 10) {
+					i++;
 					SendMessage (nextMessage);
-					nextMessage = MainClass.ConsoleReadLine ();
+					nextMessage = "This is test message number " + i.ToString ();
 				}
+				Flush ();
+				Console.WriteLine ("Done in {0}", (DateTime.UtcNow - t1).TotalMilliseconds);
 				Destroy ();
 			} catch (System.Threading.ThreadAbortException) {
 			}
@@ -46,11 +90,17 @@ namespace BD2.Test.Daemon.Chat
 
 		public override void DestroyRequestReceived ()
 		{
+			#if TRACE
+			Console.WriteLine (new System.Diagnostics.StackTrace (true).GetFrame (0));
+			#endif
 			Thread.Abort ();
 		}
 
 		public override void SessionDisconnected ()
 		{
+			#if TRACE
+			Console.WriteLine (new System.Diagnostics.StackTrace (true).GetFrame (0));
+			#endif
 			DestroyRequestReceived ();
 		}
 		#endregion
--- BD2.Test.Daemon.Chat/Program.cs
+++ BD2.Test.Daemon.Chat/Program.cs
@@ -1,3 +1,29 @@
+/*
+ * Copyright (c) 2014 Behrooz Amoozad
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the bd2 nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * */
 using System;
 using BD2.Daemon;
 using System.Collections.Generic;
@@ -15,29 +41,23 @@ namespace BD2.Test.Daemon.Chat
 				return Console.ReadLine ();
 		}
 
-		static void HandleConnection (string modeName, Guid ChatServiceAnouncementType, System.Net.Sockets.TcpClient TC)
+		static void HandleConnection (string modeName, Guid chatServiceAnouncementType, System.Net.Sockets.TcpClient TC)
 		{
 			System.IO.Stream PS = TC.GetStream ();
 			StreamHandler SH = new StreamHandler (PS);
 			ObjectBus OB = new ObjectBus (SH);
 			ServiceManager SM = new ServiceManager (OB);
 			if (modeName == "Server") {
-				SM.AnnounceService (new ServiceAnnouncement (Guid.NewGuid (), ChatServiceAnouncementType, "Chat"), ChatAgent.CreateAgent);
+				SM.AnnounceService (new ServiceAnnouncement (Guid.NewGuid (), chatServiceAnouncementType, "Chat"), ChatAgent.CreateAgent);
 			}
 			if (modeName == "Client") {
-				bool AnyAgent = false;
-				while (!AnyAgent) {
-					SortedSet<ServiceAnnouncement> RSAs = SM.EnumerateRemoteServices ();
-					foreach (ServiceAnnouncement RSA in RSAs) {
-						Console.WriteLine ("Service found: {0}", RSA.Name);
-						Console.WriteLine ("Enter Request to request service");
-						if (ConsoleReadLine () == "Request") {
-							SM.RequestService (RSA, ChatAgent.CreateAgent);
-							AnyAgent = true;
-
-						}
-						//ServiceAgent agent = new  ServiceAgent (mode, OB, SR, new Type[] { typeof(ChatMessage) });
-					}
+				System.Threading.Thread.Sleep (100);//wait for service announcement from remote
+				SortedSet<ServiceAnnouncement> RSAs = SM.EnumerateRemoteServices ();
+				foreach (ServiceAnnouncement RSA in RSAs) {
+					Console.WriteLine ("Service found: {0}", RSA.Name);
+					Console.WriteLine ("Press Enter to request service");
+					ConsoleReadLine ();
+					SM.RequestService (RSA, ChatAgent.CreateAgent);
 				}
 			}
 		}
